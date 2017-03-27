809d1100ceb9ff56690cd437c783d781d4fc4a8c
Jiří Eichler <st35931@student.upce.cz>
2017-03-27 21:59:03 +0200

    Add UserController, IUserController

---
+++ Implementation/INPDS_App/Controller/IUserController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using INPDS_App.Model;
+
+namespace INPDS_App.Controller
+{
+    public interface IUserController
+    {
+        bool IsLoggedIn();
+        User LoggedUser();
+        void Loggin(string userName, string userPassword);
+        void LogOut();
+    }
+}
+
+
