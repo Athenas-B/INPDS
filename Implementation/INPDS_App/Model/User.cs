ff4a1ae5fd5c16d4fa0278b9baa7c1c4c67d1f44
Jiří Eichler <st35931@student.upce.cz>
2017-03-27 21:13:31 +0200

    Add User model

---
+++ Implementation/INPDS_App/Model/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INPDS_App.Model
+{
+    public class User
+    {
+        
+        public int IdUser { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public Role UserRole { get; set; }
+
+
+        public User(int idUser, string userName, string password, Role userRole)
+        {
+            this.IdUser = idUser;
+            this.UserName = userName;
+            this.Password = password;
+            this.UserRole = userRole;
+        }
+
+    }
+
+    public enum Role
+    {
+        CUSTOMER,
+        ACCOUNTANT,
+        PLAYNNER
+    }
+}
