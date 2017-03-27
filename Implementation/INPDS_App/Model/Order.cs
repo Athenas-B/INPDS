398c47fe0745648e09d780eed4cdadc5368014f4
Erik Vojtech <st52854@student.upce.cz>
2017-03-27 21:27:30 +0200

    Added Order in Model.

---
+++ Implementation/INPDS_App/Model/Order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INPDS_App.Model
+{
+    public class Order
+    {
+        public User Customer { get; set; }
+        public DateTime DeliveryDeadline { get; set; }
+        public string From { get; set; }
+        public int Id { get; set; }
+        public DateTime PickupDate { get; set; }
+        public string To { get; set; }
+
+        public Order(User customer, DateTime deliveryDeadline, string @from, int id, DateTime pickupDate, string to)
+        {
+            Customer = customer;
+            DeliveryDeadline = deliveryDeadline;
+            From = @from;
+            Id = id;
+            PickupDate = pickupDate;
+            To = to;
+        }
+    }
+}
