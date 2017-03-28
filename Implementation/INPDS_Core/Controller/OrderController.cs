20a4f42b453138f1a4168d74264db180ac23ddaf
Viktor Gresak <viktor.gresak@unicornsystems.eu>
2017-03-28 15:52:31 +0200

    OrderController implementation

--- Implementation/INPDS_Core/Controller/OrderController.cs
+++ Implementation/INPDS_Core/Controller/OrderController.cs
@@ -1,4 +1,5 @@
 using System;
+using INPDS_Core.DataAccess;
 using INPDS_Core.Model;
 
 namespace INPDS_Core.Controller
@@ -7,7 +8,30 @@ namespace INPDS_Core.Controller
     {
         public void RegisterOrder(Order order)
         {
-            throw new NotImplementedException();
+            if (IsValid(order))
+            {
+                using (var context = new ReturnFreightContext())
+                {
+                    context.Orders.Add(order);
+                }
+            }
+        }
+
+        private static bool IsValid(Order order)
+        {
+            if (order.Customer == null)
+            {
+                return false;
+            }
+            if (DateTime.Now > order.DeliveryDeadline || DateTime.Now > order.PickupDate || order.PickupDate > order.DeliveryDeadline)
+            {
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(order.From) || string.IsNullOrWhiteSpace(order.To))
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
