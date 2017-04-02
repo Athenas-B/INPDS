762a4e3c07a23b3a086d30b4c7445b7aad9de44e
Erik Vojtech <st52854@student.upce.cz>
2017-04-02 17:06:36 +0200

    Added LoginView, OrderView, and WPF.Toolkit.Extended to INPDS_App.

---
+++ Implementation/INPDS_App/View/LoginView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+using INPDS_Core.Controller;
+
+namespace INPDS_App.View
+{
+    /// <summary>
+    ///     Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            IUserController userController = new UserController();
+
+            try
+            {
+                userController.Login(tbLogin.Text, tbPassword.Password);
+
+                if (userController.IsLoggedIn)
+                {
+                    var orderView = new OrderView(userController);
+                    orderView.Show();
+                    Close();
+                }
+            }
+            catch (Exception exception)
+            {
+                lbError.Content = "Unknown error";
+            }
+        }
+    }
+}
---
+++ Implementation/INPDS_App/View/OrderView.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows;
+using INPDS_Core.Controller;
+using INPDS_Core.Model;
+
+namespace INPDS_App.View
+{
+    /// <summary>
+    ///     Interaction logic for OrderView.xaml
+    /// </summary>
+    public partial class OrderView : Window
+    {
+        private readonly User _user;
+        private readonly IUserController _userController;
+
+        public OrderView()
+        {
+            InitializeComponent();
+        }
+
+        public OrderView(IUserController usrControl)
+        {
+            InitializeComponent();
+            _user = usrControl.LoggedUser;
+            _userController = usrControl;
+            Title += " | Uživatel: " + _user.UserName;
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            _userController.Logout();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            tbTo.Clear();
+            tbFrom.Clear();
+            dtpItemsReady.Text = "";
+            dtpItemsDeadline.Text = "";
+        }
+
+        private void btnConfirm_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var order = new Order(_user, (DateTime) dtpItemsDeadline.Value, tbFrom.Text,
+                    (DateTime) dtpItemsReady.Value, tbTo.Text);
+
+                IOrderController orderController = new OrderController();
+                orderController.RegisterOrder(order);
+            }
+            catch (Exception exception)
+            {
+                lbError.Content = "Unknown Error";
+            }
+        }
+    }
+}
