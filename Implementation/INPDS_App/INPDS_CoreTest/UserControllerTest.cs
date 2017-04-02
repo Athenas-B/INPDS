593414387da331ad9f049b6e98b97b21ff8f5d54
Jiří Eichler <st35931@student.upce.cz>
2017-04-02 17:10:26 +0200

    Added methods to UserControllerTest

--- Implementation/INPDS_App/INPDS_CoreTest/UserControllerTest.cs
+++ Implementation/INPDS_App/INPDS_CoreTest/UserControllerTest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using INPDS_Core.Controller;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace INPDS_CoreTest
@@ -8,8 +8,48 @@ namespace INPDS_CoreTest
     public class UserControllerTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void IsLoggedInTestTrue()
         {
+            IUserController userController = new UserController();
+            userController.Login("user", "pass");
+            Assert.IsTrue(userController.IsLoggedIn);
+        }
+
+        [TestMethod]
+        public void IsLoggedInTestFalse()
+        {
+            IUserController userController = new UserController();
+            userController.Login("user", "passs");
+            Assert.IsFalse(userController.IsLoggedIn);
+        }
+
+        [TestMethod]
+        public void LoggedUserTestIsNull()
+        {
+            IUserController userController = new UserController();
+            userController.Login("user", "passs");
+            Assert.IsNull(userController.LoggedUser);
+        }
+
+        [TestMethod]
+        public void LoggedUserTestNotNull()
+        {
+            IUserController userController = new UserController();
+            userController.Login("user", "pass");
+            Assert.IsNotNull(userController.LoggedUser);
+        }
+
+        [TestMethod]
+        public void LogOutTest()
+        {
+            IUserController userController = new UserController();
+            userController.Login("user", "pass");
+
+            Assert.IsNotNull(userController.LoggedUser);
+
+            userController.Logout();
+
+            Assert.IsNull(userController.LoggedUser);
         }
     }
 }
